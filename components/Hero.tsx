9d135a33b28e4e9eb9617e2e468fd12123639c2a
Ashutosh Verma <vashutosh625@gmail.com>
2024-06-14 23:31:55 +0530

    fixed header and footer

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,9 @@
-"use client"
+"use client";
 import React from "react";
-import {CustomButton} from "@/components"
+import { CustomButton } from "@/components";
 import Image from "next/image";
 const Hero = () => {
-    const handleScroll = ()=>{
-
-    }
+  const handleScroll = () => {};
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
@@ -16,16 +14,17 @@ const Hero = () => {
           Streamline you car rental experience with out effortless booking
           process.
         </p>
-        <CustomButton title="Explore Cars" 
-            containerStyles="bg-primary-blue text-white rounded-full mt-10"
-            handleClick={handleScroll}
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
         />
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-            <Image src="/hero.png" alt="hero" fill className="object-contain"/>
-            <div className="hero__image-overlay"></div>
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
         </div>
+        <div className="hero__image-overlay"></div>
       </div>
     </div>
   );
