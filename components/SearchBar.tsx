361d7a68a1123544723dfb39ec6594b9443eef5a
Ashutosh Verma <vashutosh625@gmail.com>
2024-06-17 23:49:52 +0530

    made search box

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -4,11 +4,14 @@ import {SearchManufacturer} from '@/components'
 import { useState } from 'react'
 
 const SearchBar = () => {
-    const [manufacturer, setManufacturer] = useState()
+  const handleSearch = ()=>{
+
+  }
+    const [manufacturer, setManufacturer] = useState('')
   return (
     <form className="searchbar" onSubmit={handleSearch}>
         <div className="searchbar__item">
-            <SearchManufacturer />
+            <SearchManufacturer manufacturer={manufacturer} />
         </div>
     </form>
   )
--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+"use client";
 
-const SearchManufacturer = () => {
+import React from "react";
+import { Combobox, Transition } from "@headlessui/react";
+import Image from "next/image";
+import { SearchManufacturerProps } from "@/types/types";
+
+const SearchManufacturer = ({
+  manufacturer,
+  setManufacturer,
+}: SearchManufacturerProps) => {
   return (
-    <div>SearchManufacturer</div>
-  )
-}
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relative w-full">
+          <Combobox.Button className="absolute top-[14px]">
+            <Image
+              src="/car-logo.svg"
+              width={20}
+              height={20}
+              className="ml-4"
+              alt="Car Logo"
+            />
+          </Combobox.Button>
+          <Combobox.Input
+            className={"search-manufacturer__input"}
+            placeholder="Vlokswagen"
+            displayValue={(manufacturer: string) => manufacturer}
+          />
+        </div>
+      </Combobox>
+    </div>
+  );
+};
 
-export default SearchManufacturer
+export default SearchManufacturer;
